adbbda16cc835fce8a7e884869c5d90fce5adcc8
FlorenciaMcapara <flornatura2903@gmail.com>
2021-11-16 17:23:41 -0300

    se resolvio conflicto

--- flor-archivo1.js
+++ flor-archivo1.js
@@ -2,13 +2,10 @@ let nombre = "Florencia"
 
 console.log(nombre)
 
-<<<<<<< HEAD
 const multiplicar = (num1, num2) => num1*num2;
 
 console.log(multiplicar(10*6));
-=======
 function suma(num1,num2){
     return num1 + num2
 }
 console.log(suma(10,20));
->>>>>>> 95b7b58988570f5466810f275ed85e30d9faaa16
